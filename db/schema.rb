2b84ecee681bedb4fe73a25afccf1c95a64193ca
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2025-05-01 10:43:46 -0700

    Add post model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_01_040257) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_01_173745) do
+  create_table "posts", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
@@ -20,4 +28,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_01_040257) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["password"], name: "index_users_on_password", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
